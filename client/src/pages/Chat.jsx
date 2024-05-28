3c74d2ae2420adc8710738382d04b1f637a0d61e
Bartu Çakır <bartuuu_lol@hotmail.com>
2024-05-28 22:03:08 +0300

    fix: trying to fix wss 3

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -16,13 +16,26 @@ export default function Chat() {
 
   useEffect(() => {
     connecToWs();
+    return () => {
+      if (ws) {
+        ws.removeEventListener("message", handleMessage);
+        ws.close();
+      }
+    };
   }, []);
 
   const connecToWs = () => {
-    const ws = new WebSocket("wss://chat-app-api-two-black.vercel.app");
-    setWs(ws);
-    ws.addEventListener("message", handleMessage);
-    ws.addEventListener("close", () => () => {
+    if (
+      ws &&
+      ws.readyState !== WebSocket.CLOSED &&
+      ws.readyState !== WebSocket.CLOSING
+    ) {
+      return;
+    }
+    const newWs = new WebSocket("wss://chat-app-api-two-black.vercel.app");
+    setWs(newWs);
+    newWs.addEventListener("message", handleMessage);
+    newWs.addEventListener("close", () => {
       setTimeout(() => {
         connecToWs();
       }, 1000);
