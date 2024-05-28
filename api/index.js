30416751afc5b2e59e7ad5ddd31a74b9b5399475
Bartu Çakır <bartuuu_lol@hotmail.com>
2024-05-28 22:41:29 +0300

    fix: vercel api deploy 2

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://chat-app-client-nine-theta.vercel.app",
+    origin: "https://stellular-khapse-92af05.netlify.app",
     credentials: true,
   })
 );
@@ -28,7 +28,7 @@ app.use(
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://chat-app-client-nine-theta.vercel.app"
+    "https://stellular-khapse-92af05.netlify.app"
   );
   res.header("Access-Control-Allow-Credentials", "true");
   next();
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { UserContextProvider } from "./contexts/UserContext";
 import { Routes } from "./routes/routes";
 function App() {
-  axios.defaults.baseURL = "https://chat-app-api-two-black.vercel.app";
+  axios.defaults.baseURL = "https://chat-app-ecru-five.vercel.app/";
   axios.defaults.withCredentials = true;
   return (
     <UserContextProvider>
