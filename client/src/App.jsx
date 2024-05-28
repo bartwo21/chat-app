ef6e4746000855761171d377088bd708515d0397
Bartu Çakır <bartuuu_lol@hotmail.com>
2024-05-28 23:42:02 +0300

    fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { UserContextProvider } from "./contexts/UserContext";
 import { Routes } from "./routes/routes";
 function App() {
-  axios.defaults.baseURL = "https://chat-app-ecru-five.vercel.app";
+  axios.defaults.baseURL = "https://chat-app-uz21.onrender.com";
   axios.defaults.withCredentials = true;
   return (
     <UserContextProvider>
